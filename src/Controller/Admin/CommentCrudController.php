0e20ff51c4da29d5e29f20090b4999e5e315ffa6
linamariaocampocuartas <linamariaocampocuartas@gmail.com>
2023-05-15 11:14:33 -0500

    Paso 9 extendido

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,13 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -18,21 +22,45 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference Comment')
+            ->setEntityLabelInPlural('Conference Comments')
+            ->setSearchFields(['author', 'text', 'email'])
+            ->setDefaultSort(['createdAt' => 'DESC'])
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('conference'))
+        ;
+    }     
+
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            FormField::addPanel('Conference'),
-            AssociationField::new('conference')->setRequired(true)->setHelp('help text'),
-            FormField::addPanel('Comment'),
-            TextField::new('author')->setHelp('Your name'),
-            TextEditorField::new('text', 'Comment')->setHelp('help text'),
-            EmailField::new('email', 'Email Address')->setHelp('Your valid email address'),
-            DateTimeField::new('createdAt'),
-            TextField::new('photoFilename')
-        ];
+        yield FormField::addPanel('Conference');
+        yield AssociationField::new('conference')->setRequired(true)->setHelp('help text');
+        yield FormField::addPanel('Comment');
+        yield TextField::new('author')->setHelp('Your name');
+        yield EmailField::new('email', 'Email Address')->setHelp('Your valid email address');
+        yield TextEditorField::new('text', 'Comment')->setHelp('help text')->hideOnIndex();
+        yield TextField::new('photoFilename');
 
-
+        $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }        
     }
-    
+   
 }
